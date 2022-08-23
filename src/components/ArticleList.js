d84d7dc0db961d3d7855893128a9401136d2eea9
Mohammed Taitai <mohammedtaitai@outlook.com>
2022-08-23 11:23:19 -0400

    commit tree 5460ae452287287031961fec644ef6cba8d55769

---
+++ src/components/ArticleList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Article from "./Article"
+
+function ArticleList({ posts }){
+
+    const articlesL = posts.map((article)=>{
+        return(
+            <Article key={article.id} title={article.title} date={article.date} preview={article.preview}/>
+        )
+    });
+    return(
+        <main>
+        {articlesL}
+        </main>
+    );
+}
+
+export default ArticleList;
